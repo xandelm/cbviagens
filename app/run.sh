fff6d4dfb00376347ef397cf40e2e98162def8e2
xandejlm <alexandrej.lima.mendes@gmail.com>
2024-03-22 21:53:07 -0300

    Add .gitignore for Python virtual environment and update run.sh script

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "Implemente aqui o script para executar a sua solução"
+#python -m venv .
+#.\Script\activate
+#pip install django
+#pip install django-rest-framework
+#pip install django-cors-headers
+#pip install djoser
+
+
+#deactivate
