0835360f58f4735467ce869255de6edf2002973c
xandejlm <alexandrej.lima.mendes@gmail.com>
2024-03-24 18:45:41 -0300

    Add requirements.txt and update run.sh script

--- app/run.sh
+++ app/run.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 
 echo "Implemente aqui o script para executar a sua solução"
-#python -m venv .
-#.\Script\activate
-#pip install django
-#pip install django-rest-framework
-#pip install django-cors-headers
-#pip install djoser
-#yarn global add @quasar/cli
-#python manage.py migrate
-#python manage.py runserver 3000
-
-
-## Update package list
-#sudo apt-get update
-
-# Install curl to fetch the Node.js setup script
-#sudo apt-get install -y curl
-
-# Fetch and execute the Node.js setup script
-#curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-
-# Install Node.js
-#sudo apt-get install -y nodejs
-
+cd app
+python -m venv .
+.\Script\activate
+#source Scripts/activate
+pip install -r requirements.txt
+cd cbviagens
+python manage.py migrate
+python manage.py load_data
+python manage.py runserver 3000 &
+cd ../cbviagens-front
+yarn install
+yarn add @quasar/cli
+yarn quasar dev
 
 #deactivate
