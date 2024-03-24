970515e844403f7a8f329c778a2d6242b010d7a7
Alexandre <alexandrej.lima.mendes@gmail.com>
2024-03-24 19:19:54 -0300

    Update run script

--- app/run.sh
+++ app/run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "Implemente aqui o script para executar a sua solução"
-cd app
+#.\Script\activate
+cd cbviagens || exit
 python -m venv .
-.\Script\activate
-#source Scripts/activate
+source Scripts/activate
+pip install Django
 pip install -r requirements.txt
-cd cbviagens
 python manage.py migrate
 python manage.py load_data
 python manage.py runserver 3000 &
-cd ../cbviagens-front
+cd ../cbviagens-front || exit
 yarn install
 yarn add @quasar/cli
 yarn quasar dev
